cd3a80aefbc37698569645fedb9f87201d7627e6
Jonhef <101654295+Jonhef@users.noreply.github.com>
2023-01-02 14:00:55 +0300

    Add files via upload

--- marketplace/userProfile/views.py
+++ marketplace/userProfile/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def profile(request):
     return render(request, 'profile.html')
+
+def login(request):
+    url = request.GET.get('url')
+    return render(request, 'reglog/index.html', context={'url':url})
