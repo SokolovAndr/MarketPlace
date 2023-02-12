adb5b1ab6969dcdfcad3d422dd2fa6b0f9be08fa
SokolovAndr <korch45@yandex.ru>
2023-02-12 13:51:05 +0300

    еще раз поправил. Не работает options.UseSqlServer

--- VkusProekt/Startup.cs
+++ VkusProekt/Startup.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Builder;
-//using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
