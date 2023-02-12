0cc362f107570c151dbd169aee3358df970bb0ee
SokolovAndr <korch45@yandex.ru>
2023-02-12 13:45:43 +0300

    начинаем подключать бд

---
+++ VkusProekt/Data/AppDBContent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VkusProekt.Data.Models;
+
+namespace VkusProekt.Data
+{
+    public class AppDBContent : DbContext
+    {
+        public AppDBContent(DbContextOptions<AppDBContent> options) : base(options) { }
+
+        public DbSet <Bludo> Bludo { get; set; }
+        public DbSet<Category> Category { get; set; }
+    }
+}
--- VkusProekt/Startup.cs
+++ VkusProekt/Startup.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VkusProekt.Data;
 using VkusProekt.Data.Interfaces;
 using VkusProekt.Data.mocks;
 
@@ -14,6 +16,11 @@ namespace VkusProekt
 {
     public class Startup
     {
+        private IConfigurationRoot _confstring;
+
+        public Startup(IHostingEnvironment hostEnv) {
+            _confstring = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -24,6 +31,8 @@ namespace VkusProekt
 
             services.AddTransient<IBludosCategory, MockCategory>();
             //интерфейс IBludosCategory реализуется в классe MockBludos
+
+            services.AddDbContext<AppDBContent>(options =>options.UseSqlServer(_confstring.GetConnectionString("DefaultConnection")));
 
             services.AddMvc(option => option.EnableEndpointRouting = false);
         }
